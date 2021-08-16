1faaf8d476466486d60eafc5d70e37b28a99c764
Tomas Carignano <tomascarignano98@gmail.com>
2021-08-16 11:38:09 -0300

    Initial commit.

---
+++ main.js
@@ -0,0 +1,11 @@
+let deckId = "";
+
+function handleClick() {
+  fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+    .then((res) => res.json())
+    .then((data) => {
+      deckId = data.deck_id;
+    });
+}
+
+document.getElementById("new-deck").addEventListener("click", handleClick);
